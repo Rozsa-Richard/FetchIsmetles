614b73b0fd18f7e0aba2b1e65584304e02fd9b84
Rikky <rozsarichardzon@gmail.com>
2025-09-10 11:03:32 +0200

    csináld át

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+const link = "https://official-joke-api.appspot.com/jokes/";
+
 async function getRandomJoke() {
     try {
-        const data = await fetch('https://official-joke-api.appspot.com/jokes/random')
-        .then(response => response.json())
-        document.getElementById("Joke").innerHTML = data.setup + " <br>" + data.punchline;
-    
+        const data = await fetch(link+"random").then(response => response.json())
+        writeOut(data);
     }
     catch (error) {
         console.log(error);
@@ -12,7 +12,29 @@ async function getRandomJoke() {
 
 async function getJokeById() {
     let id = prompt("Add meg az id-t");
-    if (nev) {
-      alert("Szia, " + nev + "!");
+    try {
+        const data = await fetch(link+ id).then(response => response.json())
+        writeOut(data);
     }
-  }
+    catch(error){
+        console.log(error)
+    }
+}
+async function getJokeByType() {
+    const type = document.getElementById("select").value;
+    try {
+        const data = await fetch(link + type +"/random").then(response => response.json())
+        writeOut(data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+function writeOut(data){
+    document.getElementById("Setup").innerHTML = data.setup;
+    document.getElementById("Punchline").innerHTML = data.punchline;
+    document.getElementById("JokeId").innerHTML = "Id: "+ data.id;
+    document.getElementById("JokeType").innerHTML = "Type: "+ data.type;
+    document.getElementById("JokeNext").onclick(getJokeById);
+    document.getElementById("JokeNext").style;
+}
